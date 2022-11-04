701962f33ad426eeb41c189696d9fe9f1697f352
Ivan <ivan.iankov@gmail.com>
2022-11-04 15:51:55 +0200

    AppImage configuration

---
+++ app-image.sh
@@ -0,0 +1,18 @@
+# jvm version number: 1, 2, 3, ...
+export JVM_VERSION=11
+
+export JVM_XMS=2G
+export JVM_XMX=6G
+export MAIN_CLASS=console.table.ConsoleTableEditorMain
+
+# array with application jar files, paths are relative to the project directory
+export JARS=("target/console-utils-1.0-jar-with-dependencies.jar" "lib/raw-console-input-1.0-jar-with-dependencies.jar")
+
+export APPLICATION_NAME=table-editor
+export IS_TERMINAL_APPLICATION=true
+
+# if empty default icon will be used
+export ICON_FILE=""
+
+# application parameters, parameters provided on AppImage run will be appended after this list
+export PARAMETERS="$(tput lines) $(tput cols)"
