69ed2ff8803294189fd4c19907aff5b25a865c0d
Ivan <ivan.iankov@gmail.com>
2022-11-11 09:21:46 +0200

    Fix

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-java -cp target/console-utils-1.0-jar-with-dependencies.jar:lib/raw-console-input-1.0-jar-with-dependencies.jar console.table.ConsoleTableEditorMain $(tput lines) $(tput cols) $@
+java -cp target/console-utils-1.0-jar-with-dependencies.jar:lib/raw-console-input-1.0-jar-with-dependencies.jar console.ConsoleTableEditorMain $(tput lines) $(tput cols) $@
