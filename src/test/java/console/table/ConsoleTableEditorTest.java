e7c07a8557ce919966c04df4812b0de30eb08b6d
Ivan <ivan.iankov@gmail.com>
2022-11-18 11:52:29 +0200

    Fix

--- src/test/java/console/table/ConsoleTableEditorTest.java
+++ src/test/java/console/table/ConsoleTableEditorTest.java
@@ -14,11 +14,11 @@ public class ConsoleTableEditorTest {
                 new TestData("test.csv", "move-down", List.of(Key.ESC), List.of(Key.F6, Key.ESC)),
                 new TestData("test.csv", "insert-after", List.of(Key.ESC), List.of(Key.F7, Key.ESC)),
                 new TestData("test.csv", "delete-row", List.of(Key.ESC), List.of(Key.F8, Key.ESC)),
-                new TestData("test.csv", "delete-column", List.of(Key.ESC), List.of(Key.CTRL_DELETE, Key.ESC))
-//                new TestData("test.csv", "cut", List.of(Key.ESC), List.of(Key.CTRL_X, Key.ESC)),
-//                new TestData("test.csv", "copy", List.of(Key.ESC), List.of(Key.CTRL_C, Key.ESC)),
-//                new TestData("test.csv", "paste", List.of(Key.ESC), List.of(Key.CTRL_V, Key.ESC)),
-//                new TestData("test.csv", "delete", List.of(Key.ESC), List.of(Key.DELETE, Key.ESC))
+                new TestData("test.csv", "delete-column", List.of(Key.ESC), List.of(Key.CTRL_DELETE, Key.ESC)),
+                new TestData("test.csv", "cut", List.of(Key.ESC), List.of(Key.CTRL_X, Key.ESC)),
+                new TestData("test.csv", "copy", List.of(Key.ESC), List.of(Key.CTRL_C, Key.ESC)),
+                new TestData("test.csv", "paste", List.of(Key.ESC), List.of(Key.CTRL_V, Key.ESC)),
+                new TestData("test.csv", "delete", List.of(Key.ESC), List.of(Key.DELETE, Key.ESC))
         );
 
         TestHelpers.testConsoleTable(
