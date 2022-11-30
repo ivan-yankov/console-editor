41e298541484c270cf81a52e38272f04f0676033
Ivan <ivan.iankov@gmail.com>
2022-11-30 16:44:00 +0200

    Install assembly with custom name

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
 mvn clean compile package install
+mvn install:install-file -Dfile=target/console-utils-assembly-latest.jar -DgroupId=yankov -DartifactId=console-utils-assembly -Dversion=latest -Dpackaging=jar
