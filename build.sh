2a71cf322c114685a48fb7b3ef386c22eaf94011
Ivan <ivan.iankov@gmail.com>
2023-03-31 13:39:17 +0300

    Improvements and fixes

--- build.sh
+++
@@ -1,2 +0,0 @@
-mvn clean compile package install
-mvn install:install-file -Dfile=target/console-utils-assembly-latest.jar -DgroupId=yankov -DartifactId=console-utils-assembly -Dversion=latest -Dpackaging=jar
